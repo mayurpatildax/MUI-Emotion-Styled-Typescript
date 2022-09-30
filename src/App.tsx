1f0b75898719e389b9e46c229eb0c871fabd441e
Mayur <mayur.patil@data-axle.com>
2022-09-30 17:28:16 +0530

    Normal Styled component

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import NormalStyled from "./components/NormalStyled";
 
 function App() {
-  return <div className="App">MUI Emotion Styled Components</div>;
+  return (
+    <>
+      <div className="App">MUI Emotion Styled Components</div>
+      <hr />
+      <NormalStyled />
+    </>
+  );
 }
 
 export default App;
--- src/components/NormalStyled.tsx
+++ src/components/NormalStyled.tsx
@@ -1,9 +1,25 @@
+import styled from "@emotion/styled";
 import React from "react";
 
 const NormalStyled = () => {
+  const StyledDiv = styled.div`
+    /* Write property names like css property names */
+    color: white;
+    font-size: large;
+    background-color: orange;
+    border: 5px solid red;
+    border-radius: 10px;
+
+    /* all units */
+    padding: 20px;
+    margin: 2rem;
+    border-bottom: 5pt solid blue;
+  `;
+
   return (
     <>
       <h3>NormalStyled</h3>
+      <StyledDiv>Styled Div</StyledDiv>
     </>
   );
 };
